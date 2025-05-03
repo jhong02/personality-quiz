65f100127ce2528586e2c7e15880873be476aeb0
jhong02 <justinhoang01@gmail.com>
2025-05-03 05:55:21 -0700

    fix refresh

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import './App.css';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Chatroom from './pages/Chatroom';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/chatroom" element={<Chatroom />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/chatroom" element={<Chatroom />} />
+    </Routes>
   );
 }
+
 
 export default App;
